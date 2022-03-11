9245d7eef1c0495a4470aceced17f2e450f57ee9
Justin Irabor <justin.irabor@gmail.com>
2022-03-12 00:39:48 +0100

    Remove propose banner

--- apps/web/pages/vote/index.tsx
+++ apps/web/pages/vote/index.tsx
@@ -49,30 +49,6 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <div className="mb-5 bg-white shadow sm:rounded-lg">
-          <div className="px-4 py-5 sm:p-6">
-            <div className="sm:flex sm:items-start sm:justify-between">
-              <div>
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
-                  Have an idea for proposal?
-                </h3>
-                <div className="mt-2 text-sm text-gray-500">
-                  <p>Submit your tutorial proposal and get paid</p>
-                </div>
-              </div>
-              <div className="mt-5 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:flex sm:items-center">
-                <IsLoggedIn>
-                  <Link href={routes.write.index} passHref>
-                    <a className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm">
-                      Propose Tutorial
-                    </a>
-                  </Link>
-                </IsLoggedIn>
-              </div>
-            </div>
-          </div>
-        </div>
-
         <InstantSearch
           searchClient={searchClient}
           indexName={process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME}
