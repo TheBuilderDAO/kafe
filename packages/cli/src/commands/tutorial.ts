320cfde169128d5b7ef0b201d377ad7a2b7f4291
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2022-03-14 13:19:12 +0700

    if case

--- packages/cli/src/commands/tutorial.ts
+++ packages/cli/src/commands/tutorial.ts
@@ -116,49 +116,94 @@ export function makeTutorialCommand() {
         });
       });
 
-      let proposalSlug: string; 
+      let proposalSlug: string;
       let proposal;
       const git = simpleGit().clean(CleanOptions.FORCE)
+      const ui = new inquirer.ui.BottomBar();
       inquirer.prompt(observe).ui.process.subscribe(async q => {
 
-        switch (q.name) {
-          case 'proposal_slug': {
-            proposalSlug = q.answer;
-            proposal = await client.getTutorialBySlug(proposalSlug);
-            log(proposal)
-            emitter.next({
-              type: "confirm",
-              name: "proposal_confirm",
-              message: `Are you sure you want to create a tutorial for ${q.answer}?`,
-            });
-            break;
-          }
-          case 'proposal_confirm': {
-            if (q.answer) {
-              const remoteOrigin = await git.remote(['get-url', 'origin'])
-              if (remoteOrigin === 'git@github.com:TheBuilderDAO/kafe.git') {
-                emitter.next({
-                  type: "confirm",
-                  name: "proposal_git_fork_confirm",
-                  message: "Looks like you didn't fork the repository yet. Do you want to contuine ?",
-                });
-              }
-              if ((await git.status()).isClean()) {
-                git.checkoutBranch(`tutorials/${proposalSlug}`, 'main')
-              } else {
-                emitter.next({
-                  type: "confirm",
-                  name: "proposal_git_confirm",
-                  message: "You have uncommitted changes. Are you sure you want to continue?",
-                });
-              }
-              // emitter.
+        if (q.name === 'proposal_slug') {
+          proposalSlug = q.answer;
+          proposal = await client.getTutorialBySlug(proposalSlug);
+          log(proposal)
+          emitter.next({
+            type: "confirm",
+            name: "proposal_confirm",
+            message: `Are you sure you want to create a tutorial for ${q.answer}?`,
+          });
+          return;
+        }
+
+        if (q.name === 'proposal_confirm') {
+          if (q.answer) {
+            if (!(await git.status()).isClean()) {
+              emitter.next({
+                type: "confirm",
+                name: "proposal_git_confirm",
+                message: "You have uncommitted changes. Are you sure you want to continue?",
+                default: false,
+              });
             }
-            break;
           }
+          return;
+        }
+
+        emitter.next({
+          type: "confirm",
+          name: "proposal_git_checkout_confirm",
+          message: `Are you confirm to checkout the branch "tutorials/${proposalSlug}" ?`,
+        })
+        if (q.name === 'proposal_git_checkout_confirm') {
+          if (q.answer) {
+            await git.checkoutBranch(`tutorials/${proposalSlug}`, 'origin/dev')
+          } else {
+            ui.log.write('Skipping checkout branch')
+          }
+          return;
+        }
+
+        ui.log.write('Creating tutorial folder')
+        await fs.mkdir(path.join(rootFolderPath, proposalSlug))
+        
+
+
+        switch (q.name) {
+          // case 'proposal_slug': {
+          //   proposalSlug = q.answer;
+          //   proposal = await client.getTutorialBySlug(proposalSlug);
+          //   log(proposal)
+          //   emitter.next({
+          //     type: "confirm",
+          //     name: "proposal_confirm",
+          //     message: `Are you sure you want to create a tutorial for ${q.answer}?`,
+          //   });
+          //   break;
+          // }
+          // case 'proposal_confirm': {
+          //   if (q.answer) {
+          //     if (!(await git.status()).isClean()) {
+          //       emitter.next({
+          //         type: "confirm",
+          //         name: "proposal_git_confirm",
+          //         message: "You have uncommitted changes. Are you sure you want to continue?",
+          //         default: false,
+          //       });
+          //     } else {
+          //       emitter.next({
+          //         type: "confirm",
+          //         name: "proposal_git_checkout_confirm",
+          //         message: `Are you confirm to checkout the branch "tutorials/${proposalSlug}" ?`,
+          //       })
+          //     }
+          //   }
+          //   break;
+          // }
+          case 'proposal_git_fork_confirm':
+          case 'proposal_git_confirm':
           case 'proposal_git_checkout_confirm': {
             if (q.answer) {
-              git.checkoutBranch(`tutorials/${proposalSlug}`, 'main')
+              await git.checkoutBranch(`tutorials/${proposalSlug}`, 'origin/dev')
+
             } else {
               emitter.complete()
             }
@@ -170,6 +215,20 @@ export function makeTutorialCommand() {
 
             } else {
               emitter.complete()
+            }
+            break;
+          }
+          case 'checkout': {
+            const remoteOrigin = await git.remote(['get-url', 'origin']) || ''
+            if (remoteOrigin === 'git@github.com:TheBuilderDAO/kafe.git') {
+              emitter.next({
+                type: "confirm",
+                name: "proposal_git_fork_confirm",
+                message: "Looks like you didn't fork the repository yet. Do you want to contuine ?",
+                default: remoteOrigin
+              });
+            } else {
+              ui.log.write(`Fork branch confirm: ${remoteOrigin} \n`);
             }
             break;
           }
