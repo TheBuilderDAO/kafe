09d56c77961b88196e1eb62d5f23482a85ea8b38
Justin Irabor <justin.irabor@gmail.com>
2022-03-15 09:29:00 +0100

    undefined coercion for input length

--- apps/web/components/FormElements/InputTextArea.tsx
+++ apps/web/components/FormElements/InputTextArea.tsx
@@ -6,9 +6,9 @@ const InputTextArea = ({ maxLength, register, name, watch }) => {
     <div>
       <p
         className={`absolute right-14 bottom-16 text-md dark:bg-kafedarker select-none bg-kafelighter shadow-sm p-2 rounded-xl ${
-          input.length / maxLength > 0.9 ? 'text-kafered' : null
+          input?.length / maxLength > 0.9 ? 'text-kafered' : null
         }`}
-      >{`${input.length || 0}/${maxLength}`}</p>
+      >{`${input?.length || 0}/${maxLength}`}</p>
 
       <textarea
         className="block w-full rounded-2xl shadow-sm focus:ring-0 dark:bg-kafedarker bg-kafelighter border-none min-h-[200px] p-5 sm:text-lg resize-none"
