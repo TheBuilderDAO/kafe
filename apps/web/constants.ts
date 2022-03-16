55b83af93d914b50ecef69200a5b190f5a690a79
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2022-03-16 16:23:41 +0700

    keep process.env.<key>

--- apps/web/constants.ts
+++ apps/web/constants.ts
@@ -1,7 +1,10 @@
 import { TutorialProgramConfig } from '@builderdao-sdk/dao-program';
 
-const getEnvOrFail = <T>(key: string, optional = false): T => {
-  const val = process.env[key];
+const getEnvOrFail = <T>(
+  val: string | number | undefined,
+  key: string,
+  optional = false,
+): T => {
   if (val) {
     return val as unknown as T;
   } else {
@@ -11,45 +14,62 @@ const getEnvOrFail = <T>(key: string, optional = false): T => {
   }
 };
 
-export const NODE_ENV = getEnvOrFail<string>('NODE_ENV');
+export const NODE_ENV = getEnvOrFail<string>(process.env.NODE_ENV, 'NODE_ENV');
 
 export const ALGOLIA_SEARCH_ADMIN_KEY = getEnvOrFail<string>(
+  process.env.ALGOLIA_SEARCH_ADMIN_KEY,
   'ALGOLIA_SEARCH_ADMIN_KEY',
 );
 export const ARWEAVE_REQUIRED_CONFIRMATIONS = 2;
-export const CERAMIC_SEED = getEnvOrFail<string>('CERAMIC_SEED');
+export const CERAMIC_SEED = getEnvOrFail<string>(
+  process.env.CERAMIC_SEED,
+  'CERAMIC_SEED',
+);
 export const NEXT_PUBLIC_ALGOLIA_APP_ID = getEnvOrFail<string>(
+  process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
   'NEXT_PUBLIC_ALGOLIA_APP_ID',
 );
 export const NEXT_PUBLIC_ALGOLIA_INDEX_NAME = getEnvOrFail<string>(
+  process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME,
   'NEXT_PUBLIC_ALGOLIA_INDEX_NAME',
 );
 export const NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY = getEnvOrFail<string>(
+  process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY,
   'NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY',
 );
 export const NEXT_PUBLIC_CERAMIC_NODE_URL = getEnvOrFail<string>(
+  process.env.NEXT_PUBLIC_CERAMIC_NODE_URL,
   'NEXT_PUBLIC_CERAMIC_NODE_URL',
 );
 export const NEXT_PUBLIC_KAFE_MINT = getEnvOrFail<string>(
+  process.env.NEXT_PUBLIC_KAFE_MINT,
   'NEXT_PUBLIC_KAFE_MINT',
 );
 export const NEXT_PUBLIC_SOLANA_NETWORK =
-  getEnvOrFail<TutorialProgramConfig.Network>('NEXT_PUBLIC_SOLANA_NETWORK');
+  getEnvOrFail<TutorialProgramConfig.Network>(
+    process.env.NEXT_PUBLIC_SOLANA_NETWORK,
+    'NEXT_PUBLIC_SOLANA_NETWORK',
+  );
 export const NEXT_PUBLIC_SOLANA_NODE_URL = getEnvOrFail<string>(
+  process.env.NEXT_PUBLIC_SOLANA_NODE_URL,
   'NEXT_PUBLIC_SOLANA_NODE_URL',
   true,
 );
 export const ZERO_ADDRESS = '11111111111111111111111111111111';
 
 export const NEXT_PUBLIC_ARWEAVE_APP_NAME = getEnvOrFail<string>(
+  process.env.NEXT_PUBLIC_ARWEAVE_APP_NAME,
   'NEXT_PUBLIC_ARWEAVE_APP_NAME',
 );
 export const NEXT_PUBLIC_ARWEAVE_HOST = getEnvOrFail<string>(
+  process.env.NEXT_PUBLIC_ARWEAVE_HOST,
   'NEXT_PUBLIC_ARWEAVE_HOST',
 );
 export const NEXT_PUBLIC_ARWEAVE_PORT = getEnvOrFail<string>(
+  process.env.NEXT_PUBLIC_ARWEAVE_PORT,
   'NEXT_PUBLIC_ARWEAVE_PORT',
 );
 export const NEXT_PUBLIC_ARWEAVE_PROTOCOL = getEnvOrFail<string>(
+  process.env.NEXT_PUBLIC_ARWEAVE_PROTOCOL,
   'NEXT_PUBLIC_ARWEAVE_PROTOCOL',
 );
