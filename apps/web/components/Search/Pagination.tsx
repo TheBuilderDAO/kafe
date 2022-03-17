9410b8916963a4d7783095c8f5d80f5f81793f5f
Justin Irabor <justin.irabor@gmail.com>
2022-03-17 22:12:29 +0100

    make pagination behave in reasonable ways

--- apps/web/components/Search/Pagination.tsx
+++ apps/web/components/Search/Pagination.tsx
@@ -1,28 +1,35 @@
 import { connectPagination } from 'react-instantsearch-dom';
 
-const Pagination = ({ currentRefinement, nbPages, refine, createURL }) => (
-  <nav
-    className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
-    aria-label="Pagination"
-  >
-    {new Array(nbPages).fill(null).map((_, index) => {
-      const page = index + 1;
+const Pagination = ({ currentRefinement, nbPages, refine, createURL }) => {
+  return (
+    <nav
+      className="relative z-0 inline-flex rounded-md shadow-sm"
+      aria-label="Pagination"
+    >
+      {new Array(nbPages).fill(null).map((_, index) => {
+        const page = index + 1;
 
-      return (
-        <a
-          key={index}
-          href={createURL(page)}
-          className="px-8 py-2 mr-2 rounded-3xl border border-kafeblack dark:border-kafewhite bg-kafelighter dark:bg-kafedarker text-sm font-medium text-kafeblack dark:text-kafewhite hover:bg-kafegold dark:hover:bg-kafegold dark:hover:text-kafeblack"
-          onClick={event => {
-            event.preventDefault();
-            refine(page);
-          }}
-        >
-          {page}
-        </a>
-      );
-    })}
-  </nav>
-);
+        return (
+          <a
+            key={index}
+            href={createURL(page)}
+            className={`px-8 py-2 mr-4 rounded-3xl  ${
+              currentRefinement === page
+                ? 'bg-kafelighter dark:bg-kafedarker'
+                : null
+            } text-sm font-medium text-kafeblack dark:text-kafewhite hover:bg-kafeblack dark:hover:bg-kafewhite dark:hover:text-kafeblack`}
+            onClick={event => {
+              event.preventDefault();
+              refine(page);
+              window.scrollTo(0, 0);
+            }}
+          >
+            {page}
+          </a>
+        );
+      })}
+    </nav>
+  );
+};
 
 export default connectPagination(Pagination);
