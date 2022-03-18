f334507322323c47941a478b3e306e033e2b71d5
Justin Irabor <justin.irabor@gmail.com>
2022-03-18 04:34:46 +0100

    DAO logo

--- apps/web/layouts/PublicLayout/LeftSidebar.tsx
+++ apps/web/layouts/PublicLayout/LeftSidebar.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import Navigation from './Navigation';
-import Image from 'next/image';
-import DAO from 'public/assets/icons/dao.svg';
+import DAOSVG from '@app/components/SVG/DAOSVG';
 
 const LeftSidebar = () => {
   return (
     <div className="w-40">
       <Navigation />
-      <div className="fixed bottom-10 left-12">
-        <Image src={DAO} width={130} height={100} alt="builder dao icon" />
-      </div>
+      <a className="fixed bottom-10 left-8" href="#">
+        <DAOSVG />
+      </a>
     </div>
   );
 };
