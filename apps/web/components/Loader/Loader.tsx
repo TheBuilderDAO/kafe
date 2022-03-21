acc34fed3acea2d4d5d5af5387de9e729ef8601e
Justin Irabor <justin.irabor@gmail.com>
2022-03-21 01:11:12 +0100

    Fixed build errors

---
+++ apps/web/components/Loader/Loader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className="w-full flex justify-center items-center">
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default Loader;
