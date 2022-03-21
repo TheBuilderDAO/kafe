74540e2b0d23e6ead9d8b69f24bf3337874178ee
Justin Irabor <justin.irabor@gmail.com>
2022-03-21 17:15:34 +0100

    Hide admin sidebar

--- apps/web/components/Admin/WriteOnGithub.tsx
+++ apps/web/components/Admin/WriteOnGithub.tsx
@@ -11,33 +11,33 @@ const WriteOnGitHub = ({
   RenderReviewer,
 }) => {
   return (
-    <div className="mt-6">
-      <RightSidebar>
-        <div className="p-6">
-          {tutorial.state !== ProposalStateE.readyToPublish &&
-            tutorial.state !== ProposalStateE.published && (
-              <div>
-                <section className="flex justify-between mb-6">
-                  <h3 className="text-2xl font-larken">Write on Github</h3>
-                  <span className="flex items-center">
-                    <Image src={Eyecon} width={18} height={18} alt="icon" />
-                    <small className="pl-2">admin</small>
-                  </span>
-                </section>
-                {tutorial.reviewer1 !== ZERO_ADDRESS && (
-                  <RenderReviewer pubkey={tutorial.reviewer1} number="1" />
-                )}
-                {tutorial.reviewer2 !== ZERO_ADDRESS && (
-                  <RenderReviewer pubkey={tutorial.reviewer2} number="2" />
-                )}
-                <IsAdmin>
+    <IsAdmin>
+      <div className="mt-6">
+        <RightSidebar>
+          <div className="p-6">
+            {tutorial.state !== ProposalStateE.readyToPublish &&
+              tutorial.state !== ProposalStateE.published && (
+                <div>
+                  <section className="flex justify-between mb-6">
+                    <h3 className="text-2xl font-larken">Write on Github</h3>
+                    <span className="flex items-center">
+                      <Image src={Eyecon} width={18} height={18} alt="icon" />
+                      <small className="pl-2">admin</small>
+                    </span>
+                  </section>
+                  {tutorial.reviewer1 !== ZERO_ADDRESS && (
+                    <RenderReviewer pubkey={tutorial.reviewer1} number="1" />
+                  )}
+                  {tutorial.reviewer2 !== ZERO_ADDRESS && (
+                    <RenderReviewer pubkey={tutorial.reviewer2} number="2" />
+                  )}
                   <AssignReviewersForm tutorial={tutorial} />
-                </IsAdmin>
-              </div>
-            )}
-        </div>
-      </RightSidebar>
-    </div>
+                </div>
+              )}
+          </div>
+        </RightSidebar>
+      </div>
+    </IsAdmin>
   );
 };
 
