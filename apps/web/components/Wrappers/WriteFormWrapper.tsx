3a5516c7d7c2d7005f43d9ffc2b2f5b43ff39a7c
Justin Irabor <justin.irabor@gmail.com>
2022-03-22 00:07:09 +0100

    checkpoint

--- apps/web/components/Wrappers/WriteFormWrapper.tsx
+++ apps/web/components/Wrappers/WriteFormWrapper.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const WriteFormWrapper = ({ children, handleSubmit, onSubmit }) => {
   return (
     <form
-      className="flex flex-col lg:flex-row lg:justify-between text-kafeblack dark:text-kafewhite lg:-mt-10 mt-10 px-4 mb-32 lg:mb-0"
+      className="flex items-start justify-between w-full"
       onSubmit={handleSubmit(onSubmit)}
     >
       {children}
