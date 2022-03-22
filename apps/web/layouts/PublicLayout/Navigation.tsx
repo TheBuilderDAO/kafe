d7e5f61dfc0388c17554bc5166abf612930c2409
Justin Irabor <justin.irabor@gmail.com>
2022-03-22 01:05:35 +0100

    Left sidebar weirdness gone

--- apps/web/layouts/PublicLayout/Navigation.tsx
+++ apps/web/layouts/PublicLayout/Navigation.tsx
@@ -13,43 +13,35 @@ const Navigation = () => {
   const StyledNavigate: React.FC<NavigateProps> = tw.div`
     ${props =>
       props.$active
-        ? 'font-larken-italic dark:bg-kafedarker bg-kafelighter dark:text-kafewhite text-kafeblack rounded-3xl w-[100px]'
+        ? 'font-larken-italic dark:bg-kafedarker bg-kafelighter dark:text-kafewhite text-kafeblack rounded-3xl w-24'
         : ''}
     lg:text-2xl
     text-2xl
     font-larken
-    block
-    dark:hover:bg-kafedarker hover:bg-kafelighter dark:hover:text-kafewhite dark:text-kafewhite hover:text-kafeblack hover:text-kafewhite py-2 rounded-3xl w-32
+    cursor-pointer
+    dark:hover:bg-kafedarker hover:bg-kafelighter dark:hover:text-kafewhite dark:text-kafewhite hover:text-kafeblack hover:text-kafewhite py-1 rounded-2xl w-24 text-center -ml-5
   `;
 
   return (
-    <>
-      <div className="flex flex-col flex-1 mt-5 h-10 lg:h-full">
-        <nav>
-          <div>
-            <Link key="learn" href={routes.learn.index} passHref>
-              <StyledNavigate $active={router.pathname === routes.learn.index}>
-                Learn
-              </StyledNavigate>
-            </Link>
-          </div>
-          <div>
-            <Link key="vote" href={routes.vote.index} passHref>
-              <StyledNavigate $active={router.pathname === routes.vote.index}>
-                Vote
-              </StyledNavigate>
-            </Link>
-          </div>
-          <div>
-            <Link key="write" href={routes.write.index} passHref>
-              <StyledNavigate $active={router.pathname === routes.write.index}>
-                Write
-              </StyledNavigate>
-            </Link>
-          </div>
-        </nav>
-      </div>
-    </>
+    <div className="mt-5">
+      <nav className="space-y-1">
+        <Link key="learn" href={routes.learn.index} passHref>
+          <StyledNavigate $active={router.pathname === routes.learn.index}>
+            Learn
+          </StyledNavigate>
+        </Link>
+        <Link key="vote" href={routes.vote.index} passHref>
+          <StyledNavigate $active={router.pathname === routes.vote.index}>
+            Vote
+          </StyledNavigate>
+        </Link>
+        <Link key="write" href={routes.write.index} passHref>
+          <StyledNavigate $active={router.pathname === routes.write.index}>
+            Write
+          </StyledNavigate>
+        </Link>
+      </nav>
+    </div>
   );
 };
 
