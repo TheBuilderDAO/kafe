d01560a390c6a6525b2750792ab16bfff6aaa25b
Justin Irabor <justin.irabor@gmail.com>
2022-03-22 21:13:09 +0100

    index page prettification

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,31 +1,41 @@
 import React from 'react';
 import Head from 'next/head';
 import { NextPage } from 'next';
-import TutorialProposalHit from '@app/components/Search/TutorialProposalHit';
-import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, Hits, Configure } from 'react-instantsearch-dom';
-import Link from 'next/link';
+import Gateway from '@app/components/SVG/Gateway';
 import routes from 'routes';
-import {
-  NEXT_PUBLIC_ALGOLIA_APP_ID,
-  NEXT_PUBLIC_ALGOLIA_INDEX_NAME,
-  NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY,
-} from '@app/constants';
-import Loader from '@app/components/Loader/Loader';
-
-const searchClient = algoliasearch(
-  NEXT_PUBLIC_ALGOLIA_APP_ID,
-  NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY,
-);
+import HomeCTA from '@app/components/Index/HomeCTA';
 
 const LandingPage: NextPage = () => {
   return (
-    <>
+    <div className="relative text-kafeblack dark:text-kafewhite min-h-[1200px] w-[900px]">
       <Head>
         <title>Builder DAO</title>
       </Head>
-      <Loader message="Work in Progress" />
-    </>
+      <main>
+        <Gateway />
+        <div className="absolute top-96 left-20 ">
+          <HomeCTA
+            headline="Learn from guides written by our community"
+            path={routes.learn.index}
+            cta="view all guides"
+          />
+        </div>
+        <div className="absolute top-[650px] right-0">
+          <HomeCTA
+            headline="Vote on proposals for guides you want to be written"
+            path={routes.vote.index}
+            cta="view all proposals"
+          />
+        </div>
+        <div className="absolute bottom-0 left-20">
+          <HomeCTA
+            headline="Create your own proposal, and get paid in $KAFE to write"
+            path={routes.write.index}
+            cta="create a proposal"
+          />
+        </div>
+      </main>
+    </div>
   );
 };
 
