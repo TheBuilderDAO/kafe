7a6195e7297a0fedd4788efa4c2dd543fbfe76cb
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2022-03-24 00:35:18 +0700

    fix

--- apps/web/components/Tags/Tags.tsx
+++ apps/web/components/Tags/Tags.tsx
@@ -10,7 +10,7 @@ const Tags = (props): JSX.Element => {
           key={tag}
           className="inline-flex items-center px-2 py-0 m-2 ml-0 rounded-sm text-[10px] font-extralight dark:bg-[#2A2829] text-kafeblack dark:text-kafewhite bg-kafelighter"
         >
-          {tag?.value?.toUpperCase() || tag.toUpperCase()}
+          {tag?.value?.toUpperCase() || tag?.toUpperCase()}
         </span>
       ))}
     </span>
