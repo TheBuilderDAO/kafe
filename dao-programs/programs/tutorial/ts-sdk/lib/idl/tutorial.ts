5478de5ff11210f193675d8fb69c8ef5fad56289
Justin Irabor <justin.irabor@gmail.com>
2022-03-24 06:34:34 +0100

    LR-239

--- dao-programs/programs/tutorial/ts-sdk/lib/idl/tutorial.ts
+++ dao-programs/programs/tutorial/ts-sdk/lib/idl/tutorial.ts
@@ -765,7 +765,7 @@ export type Tutorial = {
     {
       code: 6012;
       name: 'NotEnoughSolError';
-      msg: 'Error: Cannot tips: not enough SOL';
+      msg: 'Error: Cannot tip: not enough SOL';
     },
     {
       code: 6013;
