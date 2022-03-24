3ec4ac3bf788440d04f98710f8ae6c718f144582
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2022-03-24 22:15:21 +0700

    set limit of rendered tags & show them in hover state.

--- apps/web/components/Tags/Tags.tsx
+++ apps/web/components/Tags/Tags.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 
-const Tags = (props): JSX.Element => {
-  let { tags } = props;
+interface Props {
+  tags: string[] | { value: string }[];
+  max?: number;
+}
 
+const Tags: React.FC<Props> = ({ tags, max = 4 }) => {
   return (
-    <span>
-      {tags.map(tag => (
-        <span
-          key={tag}
-          className="inline-flex items-center px-2 py-0 m-2 ml-0 rounded-sm text-[10px] font-extralight dark:bg-[#2A2829] text-kafeblack dark:text-kafewhite bg-kafelighter"
-        >
-          {tag?.value?.toUpperCase() || tag.toUpperCase()}
-        </span>
+    <span className="relative flex flex-row flex-wrap cursor-pointer group">
+      {tags.map((tag, index) => (
+        <>
+          {tags.length > max && index === max ? (
+            <span className="my-auto text-xs transition duration-300 ease-in opacity-75 group-hover:opacity-0 group-hover:hidden text-kafeblack dark:text-kafewhite ">
+              +{tags.length - max}
+            </span>
+          ) : null}
+          <span
+            key={tag}
+            className={`${
+              index >= max
+                ? 'opacity-0  group-hover:opacity-100 group-hover:block'
+                : ''
+            } inline-flex items-center px-2 py-0 m-2 ml-0 rounded-sm text-[10px] font-extralight dark:bg-[#2A2829] text-kafeblack dark:text-kafewhite bg-kafelighter transition ease-in duration-400`}
+          >
+            {tag?.value?.toUpperCase() || tag?.toUpperCase()}
+          </span>
+        </>
       ))}
     </span>
   );
