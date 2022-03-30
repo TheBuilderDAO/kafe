8320f68bb292d02828b803a1d88dbad86f11bd86
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2022-03-30 17:43:42 +0700

    replace with loader

--- apps/web/components/Search/GuideHit.tsx
+++ apps/web/components/Search/GuideHit.tsx
@@ -5,34 +5,12 @@ import Link from 'next/link';
 import { useGetListOfTippersById } from '@builderdao-sdk/dao-program';
 import ImageStack from '@app/components/ImageStack';
 import React from 'react';
+import Loader from '../Loader/Loader';
 
 const TippersInfo = ({ id }) => {
   const { tippers, loading, error } = useGetListOfTippersById(id);
   if (loading) {
-    return (
-      <div className="flex items-center justify-center">
-        <svg
-          className="w-5 h-5 mr-3 -ml-1 text-white animate-spin"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <circle
-            className="opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-          ></circle>
-          <path
-            className="opacity-75"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-          ></path>
-        </svg>
-        Loading
-      </div>
-    );
+    return <Loader message="Loading..." />;
   } else if (error) {
     return <div>Error</div>;
   }
