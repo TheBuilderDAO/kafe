fbf59d4af790a02efdc5cfa65ff05db300d4b5fc
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2022-03-31 19:12:44 +0700

    make github download work

--- packages/md-formatter/src/github.ts
+++ packages/md-formatter/src/github.ts
@@ -1,12 +1,12 @@
 
 import axios from 'axios';
 
-const gh_token = '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';
+const ghToken = '_TODO_';
 
 export const getFileFromGithub = async (url: string) => {
   const file = await axios.get(url, {
     headers: {
-      Authorization: `token ${gh_token}`,
+      Authorization: `token ${ghToken}`,
     },
   });
   return file.data;
