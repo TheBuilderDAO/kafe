6784684affca4599eedd75df37db8569e7db6c39
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2022-04-12 18:42:58 +0700

    change the way determining installed globally

--- packages/cli/src/constants.ts
+++ packages/cli/src/constants.ts
@@ -1,5 +1,7 @@
 import path from 'path';
+import isInstalledGlobally from 'is-installed-globally';
 
-export const rootTutorialFolderPath = path.resolve(__dirname).includes('yarn/global')
+
+export const rootTutorialFolderPath = isInstalledGlobally
   ? path.join(process.cwd(), 'tutorials')
   : path.join(__dirname, '../../../', 'tutorials');
