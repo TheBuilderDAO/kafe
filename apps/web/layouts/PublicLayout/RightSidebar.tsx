5b87f13265329a16e938396f5113ad8a634d5a31
silentlight <bartoszhejman@gmail.com>
2022-04-15 19:58:03 +0200

    Fix top margin (#161)

--- apps/web/layouts/PublicLayout/RightSidebar.tsx
+++ apps/web/layouts/PublicLayout/RightSidebar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const RightSidebar = ({ children }) => {
   return (
-    <div className="mt-0 ml-3 text-sm dark:bg-kafedarker bg-kafelighter rounded-2xl w-menu h-fit ">
+    <div className="mt-10 ml-3 text-sm dark:bg-kafedarker bg-kafelighter rounded-2xl w-menu h-fit ">
       {children}
     </div>
   );
