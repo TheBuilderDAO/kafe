fff9ed1fd3f7b3c1bdbea440aaa328e3104af3a7
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2022-04-26 13:59:26 +0300

    add tailwind

---
+++ apps/docs/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './components/**/*.{jsx,tsx}',
+    './pages/**/*.md',
+    './pages/**/*.mdx',
+    './theme.config.js',
+    '../../packages/ui/src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
