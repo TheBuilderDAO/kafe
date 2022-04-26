08ee9b5a1cea086888ef96d99104611b3c8d99e8
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2022-04-26 14:21:10 +0300

    add sidebar meta.json for sub folders

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -1,3 +1,5 @@
+const { colors } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './components/**/*.{jsx,tsx}',
@@ -6,8 +8,24 @@ module.exports = {
     './theme.config.js',
     '../../packages/ui/src/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.kafeblack,
+        kafeblack: '#1E1C1E',
+        kafewhite: '#EAE4D9',
+        kafepurple: '#9462F7',
+        kafegold: '#EFBB73',
+        kafered: '#EB5F49',
+        kafedarker: '#131213',
+        kafelighter: '#FCFBF9',
+        kafemellow: '#B2ADA4',
+        kafedarkred: '#A24A5B',
+        kafedarkpurple: '#B930B4',
+        kafedarkgold: '#BA8D57',
+      },
+    },
   },
   plugins: [],
 };
--- apps/docs/theme.config.js
+++ apps/docs/theme.config.js
@@ -14,7 +14,7 @@ export default {
   logo: (
     <>
       <svg>...</svg>
-      <span>Next.js Static Site Generator</span>
+      <span>BuilderDAO Docs</span>
     </>
   ),
   head: (
