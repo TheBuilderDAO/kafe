1103560f07742e771c2534192abda844700ad095
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2022-04-26 13:01:22 +0300

    update content script to cleanup removed folders

--- apps/web/scripts/content.js
+++ apps/web/scripts/content.js
@@ -1,5 +1,4 @@
 const fs = require('fs-extra');
-const { resolve } = require('path');
 const path = require('path');
 
 async function main() {
@@ -17,33 +16,30 @@ async function main() {
       return true;
     }
     return false;
-
-    // return new Promise((res, rej) => {
-    //     if (isDirectory || src.endsWith('.json')) {
-    //         resolve(true)
-    //     }
-    //     else resolve(false)
-    // })
-    // if (src.includes('.json')) {
-    //     return true
-    // }
-    // return false
   };
   const publicTutorialFolder = path.join('./public/tutorials');
   await fs.ensureDir(publicTutorialFolder);
   for (const tutorial of tutorials) {
     const source = path.join(tutorialFolder, tutorial);
     const target = path.join(publicTutorialFolder, tutorial);
-    await fs.copy(
-      path.join(source, 'builderdao.lock.json'),
-      path.join(target, 'builderdao.lock.json'),
-      { filter: filterFunc },
-    );
-    await fs.copy(
-      path.join(source, 'builderdao.config.json'),
-      path.join(target, 'builderdao.config.json'),
-      { filter: filterFunc },
-    );
+    const lockFile = path.join(source, 'builderdao.lock.json');
+    const configFile = path.join(source, 'builderdao.config.json');
+    const packageFile = path.join(source, 'package.json');
+    const files = await Promise.all([
+      await fs.pathExists(lockFile),
+      await fs.pathExists(configFile),
+      await fs.pathExists(packageFile),
+    ]);
+    if (!files.every(Boolean)) {
+      await fs.remove(source);
+      continue;
+    }
+    await fs.copy(lockFile, path.join(target, 'builderdao.lock.json'), {
+      filter: filterFunc,
+    });
+    await fs.copy(configFile, path.join(target, 'builderdao.config.json'), {
+      filter: filterFunc,
+    });
     await fs.copy(
       path.join(source, 'package.json'),
       path.join(target, 'package.json'),
